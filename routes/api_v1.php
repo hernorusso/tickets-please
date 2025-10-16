b092c0b5c6c50a9ab4bf56963a07b6ee94b1d04b
herno <hernan.russo@stensul.com>
2025-10-16 20:39:00 -0300

    Set up ticket Model and controller Set DB seeder Versioning the Api

---
+++ routes/api_v1.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\v1\TicketController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\json;
+
+
+Route::get('tickets', [TicketController::class, 'index']);
