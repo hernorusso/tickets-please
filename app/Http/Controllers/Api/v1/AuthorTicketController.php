fa71b5557d1ff225e0618174dc8828dd706b50d5
herno <hernan.russo@stensul.com>
2025-12-19 14:29:51 -0300

    get tiekets by author

--- app/Http/Controllers/Api/v1/AuthorTicketController.php
+++ app/Http/Controllers/Api/v1/AuthorTicketController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\V1\TicketFilter;
+use App\Http\Resources\V1\TicketResource;
+use App\Models\Ticket;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class AuthorTicketController extends Controller
 {
-    //
+    public function index($authorId, TicketFilter $filters): AnonymousResourceCollection
+    {
+        return TicketResource::collection(Ticket::query()
+            ->where('user_id', $authorId)
+            ->filter($filters)
+            ->paginate());
+    }
 }
