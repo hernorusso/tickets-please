2325e4f937787059b9183ad7775d2caa0063b4f6
herno <hernan.russo@stensul.com>
2025-12-19 11:33:09 -0300

    Add ticket by author contoller

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthorTicketController;
 use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\Api\V1\AuthorController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use function Pest\Laravel\json;
-
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketController::class);
