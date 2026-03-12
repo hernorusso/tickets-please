f79aecbba883f4d8b29707722eddd7b693dfe3eb
herno <hernan.russo@stensul.com>
2026-03-12 17:50:25 -0300

    improve author ticket contoller

--- app/Http/Controllers/Api/v1/AuthorTicketController.php
+++ app/Http/Controllers/Api/v1/AuthorTicketController.php
@@ -5,16 +5,13 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Resources\V1\TicketResource;
-use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class AuthorTicketController extends Controller
 {
-    public function index($authorId, TicketFilter $filters): AnonymousResourceCollection
+    public function index(User $author, TicketFilter $filters): AnonymousResourceCollection
     {
-        return TicketResource::collection(Ticket::query()
-            ->where('user_id', $authorId)
-            ->filter($filters)
-            ->paginate());
+        return TicketResource::collection($author->tickets()->filter($filters)->paginate());
     }
 }
