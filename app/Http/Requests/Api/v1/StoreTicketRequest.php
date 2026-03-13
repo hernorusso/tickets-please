e5e6b8a8f9e94f20adc20b022f5ea40edd97081a
herno <hernan.russo@stensul.com>
2026-03-13 12:15:27 -0300

    Post Ticket: validate input data

--- app/Http/Requests/Api/v1/StoreTicketRequest.php
+++ app/Http/Requests/Api/v1/StoreTicketRequest.php
@@ -11,7 +11,7 @@ class StoreTicketRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,18 @@ class StoreTicketRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'data.attributes.title' => 'required|string',
+            'data.attributes.description' => 'required|string',
+            'data.attributes.status' => 'required|in:A,C,H,X',
+            'data.relationships.author.data.id' => 'required|integer',
+
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'data.attributes.status' => 'The status must be one of: A, C, H, X',
         ];
     }
 }
